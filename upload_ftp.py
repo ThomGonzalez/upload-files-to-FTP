a61fb0ffa7ce057a36c6c5abf70ef4efdeddc512
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-08-19 18:13:31 -0500

    Se creó método simple_upload solo sube un archivo al servidor.

--- upload_ftp.py
+++ upload_ftp.py
@@ -5,11 +5,11 @@ import os
 
 class Base(object):
 	'''
-	Datos FTP
+	Datos FTP para subir archivos al servidor.
 	'''
-	_ftp_servidor = 'thomgonzalez.com'
-	_ftp_usuario = 'thomgonzalez'
-	_ftp_password = 'th0mg.15'
+	_ftp_servidor = 'server'
+	_ftp_usuario = 'user'
+	_ftp_password = 'pass'
 	_ftp_raiz = '/public_html'
 
 	def __init__(self, file_names=None):
@@ -17,20 +17,29 @@ class Base(object):
 		self.conexion()
 
 	def conexion(self):
-		''' 
-		Conectamos con el servidor 
-		'''
+		''' Conectamos con el servidor '''
 		try:
 			self.server = ftplib.FTP(self._ftp_servidor, self._ftp_usuario, self._ftp_password)
 		except:
-			print('No se ha podido conectar al servidor: '+self._ftp_servidor) 
+			print('No se ha podido conectar al servidor: '+self._ftp_servidor)
+
+	def simple_upload(self):
+		try:
+			files = open(self.file_names, 'rb')
+			self.server.cwd(self._ftp_raiz)
+			self.server.storbinary('STOR '+ 'fichero.zip', files)
+			files.close()
+			self.server.quit()
+		except:
+			print('No se ha podido encontrar el fichero: '+self.file_names)
+		return True 
 
 	def multiple_upload(self):
 		try:
 			for fichero in self.file_names:
 				files = open(fichero['archivo'], 'rb')
 				self.server.cwd(self._ftp_raiz)
-				self.server.storbinary('STOR: '+fichero['nombre'], files)
+				self.server.storbinary('STOR '+fichero['nombre'], files)
 				files.close()
 
 			self.server.quit()
@@ -50,11 +59,11 @@ class UploadFile(Base):
 	def data(self):
 	    return self._estatus
 	
-# Ruta del fichero
+# Lista de ficheros
 ficheros = [
 	{'nombre': 'apache1.zip', 'archivo': r'C:/apache-maven-3.2.5-bin.zip'},
 	{'nombre': 'apache2.zip', 'archivo': r'C:/apache-maven-3.3.1-bin.zip'}
 ]
 
-upload = UploadFile(file_names=ficheros).data
+upload = UploadFile(file_names=files).data
 print(upload)
